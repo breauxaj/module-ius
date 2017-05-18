6d6f2c07296caeb363f51f99779fead27fc5c442
Adam Breaux <breauxaj@gmail.com>
2017-05-18 17:25:36 -0400

    new scaffolding

--- manifests/params.pp
+++ manifests/params.pp
@@ -9,68 +9,45 @@
 class ius::params {
   $ius_package_ensure = 'latest'
 
-  case $::operatingsystem {
-    'CentOS', 'OracleLinux', 'RedHat', 'Scientific': {
+  case $::osfamily {
+    'RedHat': {
       $ius_package = 'ius-release'
 
-      case $::operatingsystemmajrelease {
-        '6': {
-          if ( !$::ius_installed ) {
-            yumrepo { 'ius-temp':
-              mirrorlist     => 'https://mirrors.iuscommunity.org/mirrorlist?repo=ius-centos6&arch=$basearch&protocol=http',
-              failovermethod => 'priority',
-              enabled        => '1',
-              gpgcheck       => '1',
-              gpgkey         => 'file:///etc/pki/rpm-gpg/IUS-COMMUNITY-GPG-KEY',
-              descr          => 'IUS Community Packages for Enterprise Linux 6 - $basearch',
-              require        => File['/etc/pki/rpm-gpg/IUS-COMMUNITY-GPG-KEY'],
-            }
-
-            file { '/etc/pki/rpm-gpg/IUS-COMMUNITY-GPG-KEY':
-              ensure => present,
-              owner  => 'root',
-              group  => 'root',
-              mode   => '0644',
-              source => 'puppet:///modules/ius/IUS-COMMUNITY-GPG-KEY',
-            }
-          } else {
-            yumrepo { 'ius-temp':
-              ensure => absent
-            }
-          }
-        }
-        '7': {
-          if ( !$::ius_installed ) {
-            yumrepo { 'ius-temp':
-              mirrorlist     => 'https://mirrors.iuscommunity.org/mirrorlist?repo=ius-centos7&arch=$basearch&protocol=http',
-              failovermethod => 'priority',
-              enabled        => '1',
-              gpgcheck       => '1',
-              gpgkey         => 'file:///etc/pki/rpm-gpg/IUS-COMMUNITY-GPG-KEY',
-              descr          => 'IUS Community Packages for Enterprise Linux 7 - $basearch',
-              require        => File['/etc/pki/rpm-gpg/IUS-COMMUNITY-GPG-KEY'],
-            }
-
-            file { '/etc/pki/rpm-gpg/IUS-COMMUNITY-GPG-KEY':
-              ensure => present,
-              owner  => 'root',
-              group  => 'root',
-              mode   => '0644',
-              source => 'puppet:///modules/ius/IUS-COMMUNITY-GPG-KEY',
-            }
-          } else {
-            yumrepo { 'ius-temp':
-              ensure => absent
-            }
-          }
-        }
+      case $::operatingsystem {
         default: {
-          fail("The ${module_name} module is not supported on an ${::operatingsystem} ${::operatingsystemmajrelease} distribution.")
+          case $::operatingsystemmajrelease {
+            default: {
+              if ( !$::ius_installed ) {
+                file { '/etc/pki/rpm-gpg/IUS-COMMUNITY-GPG-KEY':
+                  ensure => present,
+                  owner  => 'root',
+                  group  => 'root',
+                  mode   => '0644',
+                  source => 'puppet:///modules/ius/IUS-COMMUNITY-GPG-KEY',
+                }
+
+                yumrepo { 'ius-temp':
+                  mirrorlist     => 'https://mirrors.iuscommunity.org/mirrorlist?repo=ius-centos7&arch=$basearch&protocol=http',
+                  failovermethod => 'priority',
+                  enabled        => '1',
+                  gpgcheck       => '1',
+                  gpgkey         => 'file:///etc/pki/rpm-gpg/IUS-COMMUNITY-GPG-KEY',
+                  descr          => 'IUS Community Packages for Enterprise Linux 7 - $basearch',
+                  require        => File['/etc/pki/rpm-gpg/IUS-COMMUNITY-GPG-KEY'],
+                }
+              } else {
+                yumrepo { 'ius-temp':
+                  ensure => absent
+                }
+              }
+            }
+          }
         }
       }
     }
     default: {
-      fail("The ${module_name} module is not supported on an ${::operatingsystem} based system.")
+      fail("The ${module_name} module is not supported on an ${::osfamily} based system.")
     }
   }
+
 }
